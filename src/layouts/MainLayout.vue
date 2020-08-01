f0ac6fae8debb0ccce396dd8fce1db09411767c1
Kamil Mikolajczyk <rego@gazeta.pl>
2020-08-01 23:34:42 +0200

    added MainLayout layout component

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,125 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-list-group
+            v-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.icon"
+            append-icon="mdi-chevron-down"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ item.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item
+              v-for="(child, i) in item.children"
+              :key="i"
+              link
+            >
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item
+            v-else
+            :key="item.text"
+            link
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">PoPanda </span>
+        <v-icon>mdi-panda</v-icon>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        large
+      >
+        <v-avatar
+          size="32px"
+          item
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          ></v-img></v-avatar>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'MainLayout',
+  data: () => ({
+    drawer: false,
+    items: [
+      { icon: 'mdi-calendar', text: 'Schedule' },
+      {
+        icon: 'mdi-cog',
+        text: 'Settings',
+        model: true,
+        children: [
+          { text: 'People', icon: 'mdi-account-edit' },
+          { text: 'Teams', icon: 'mdi-account-group' },
+          { text: 'Activity types', icon: 'mdi-format-list-checks' },
+        ],
+      },
+    ],
+  }),
+});
+</script>
