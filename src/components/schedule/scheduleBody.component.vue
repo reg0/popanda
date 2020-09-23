546822b99655b35a4780cd7e1f3b10cd0756b812
Kamil Mikolajczyk <rego@gazeta.pl>
2020-09-23 23:28:29 +0200

    extracted scheduleCell component

--- src/components/schedule/scheduleBody.component.vue
+++ src/components/schedule/scheduleBody.component.vue
@@ -12,23 +12,11 @@
         {{ person.lastName.replaceAll(/[\s]*-[\s]*/g, '&#8209;') }}
         {{ person.firstName.replaceAll(/[\s]/g, '&#0160;') }}
       </td>
-      <td v-for="activity in activities[person.id]" :key="activity.id" :class="{holiday: holidays.indexOf(activity.isoDate) >= 0}">
-        <div class="cell-icons">
-        <v-tooltip top v-if="activity.commentForSuperior || activity.commentPublic">
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon v-bind="attrs" v-on="on" x-small color="normal">mdi-comment-alert-outline</v-icon>
-          </template>
-          <div>
-            <span v-if="activity.commentPublic">{{ activity.commentPublic }}</span>
-            <br v-if="activity.commentForSuperior && activity.commentPublic" />
-            <span v-if="activity.commentForSuperior">{{ activity.commentForSuperior }}</span>
-          </div>
-        </v-tooltip>
-        </div>
-        <div class="cell-color" :style="{backgroundColor: activity.type.color, color: fontColorForBg(activity.type.color)}">
-          <span class="dayNumber">{{ getDayNumber(activity.isoDate) }}</span>
-        </div>
-      </td>
+      <ScheduleCell v-for="activity in activities[person.id]"
+        :activity="activity"
+        :key="activity.id"
+        :iSholiday="holidays.indexOf(activity.isoDate) >= 0"
+      />
     </tr>
   </tbody>
 </template>
@@ -38,10 +26,13 @@ import Vue from 'vue';
 import { IActivity } from '@/interfaces/models/actvity.model.interface';
 import { IPerson } from '@/interfaces/models/person.model.interface';
 import { MyDate } from '@/utils/date.utils';
-import { fontColorForBackground } from '@/utils/color.utils';
+import ScheduleCell from './scheduleCell.component.vue';
 
 export default Vue.extend({
   name: 'ScheduleHeader',
+  components: {
+    ScheduleCell,
+  },
   props: {
     people: {
       type: Array as () => IPerson[],
@@ -54,17 +45,6 @@ export default Vue.extend({
     },
     holidays: {
       type: Array as () => string[],
-    },
-  },
-  data: () => ({
-    show: true,
-  }),
-  methods: {
-    fontColorForBg(bgColor: string) {
-      return fontColorForBackground(bgColor);
-    },
-    getDayNumber(isoDate: string) {
-      return new MyDate(isoDate).day;
     },
   },
 });
---
+++ src/components/schedule/scheduleCell.component.vue
@@ -0,0 +1,42 @@
+<template>
+  <td :key="activity.id" :class="{holiday: isHoliday}">
+    <div class="cell-icons">
+    <v-tooltip top v-if="activity.commentForSuperior || activity.commentPublic">
+      <template v-slot:activator="{ on, attrs }">
+        <v-icon v-bind="attrs" v-on="on" x-small color="normal">mdi-comment-alert-outline</v-icon>
+      </template>
+      <div>
+        <span v-if="activity.commentPublic">{{ activity.commentPublic }}</span>
+        <br v-if="activity.commentForSuperior && activity.commentPublic" />
+        <span v-if="activity.commentForSuperior">{{ activity.commentForSuperior }}</span>
+      </div>
+    </v-tooltip>
+    </div>
+    <div class="cell-color" :style="{backgroundColor: activity.type.color, color: fontColorForBg(activity.type.color)}">
+      <span class="dayNumber">{{ getDayNumber(activity.isoDate) }}</span>
+    </div>
+  </td>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { IActivity } from '@/interfaces/models/actvity.model.interface';
+import { MyDate } from '@/utils/date.utils';
+import { fontColorForBackground } from '@/utils/color.utils';
+
+export default Vue.extend({
+  name: 'ScheduleCell',
+  props: {
+    activity: Object as () => IActivity,
+    isHoliday: Boolean,
+  },
+  methods: {
+    fontColorForBg(bgColor: string) {
+      return fontColorForBackground(bgColor);
+    },
+    getDayNumber(isoDate: string) {
+      return new MyDate(isoDate).day;
+    },
+  },
+});
+</script>
